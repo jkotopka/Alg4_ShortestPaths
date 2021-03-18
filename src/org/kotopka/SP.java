9495db6788c5189f088d2da97a8f46dd545c3c86
John Kotopka <jkotopka@gmail.com>
2021-03-18 17:55:27 -0500

    add SP.java class skeleton

---
+++ src/org/kotopka/SP.java
@@ -0,0 +1,21 @@
+package org.kotopka;
+
+public class SP {
+
+    public SP(EdgeWeightedDigraph G, int s) {
+
+    }
+
+    public double distTo(int v) {
+
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+
+    }
+
+    public boolean hasPathTo(int v) {
+
+    }
+
+}
