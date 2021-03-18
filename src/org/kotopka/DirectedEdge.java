549f7531408b363020414ef0e268cd0d36e6f17f
John Kotopka <jkotopka@gmail.com>
2021-03-18 17:26:48 -0500

    update DirectedEdge.java

--- src/org/kotopka/DirectedEdge.java
+++ src/org/kotopka/DirectedEdge.java
@@ -1,21 +1,42 @@
 package org.kotopka;
 
+/**
+ * {@code DirectedEdge} - Used to represent a directed edge in a weighted digraph
+ */
 public class DirectedEdge {
 
     private final int v;
     private final int w;
     private final double weight;
 
+    /**
+     * {@code DirectedEdge} data type constructor
+     * @param v the origin vertex of this edge
+     * @param w the destination vertex of this edge
+     * @param weight the weight of this edge
+     */
     public DirectedEdge(int v, int w, double weight) {
         this.v = v;
         this.w = w;
         this.weight = weight;
     }
 
+    /**
+     * {@code from()} - returns the origin vertex of this edge
+     * @return origin vertex
+     */
     public int from() { return v; }
 
+    /**
+     * {@code to()} - returns the destination vertex of this edge
+     * @return destination vertex
+     */
     public int to() {  return w; }
 
+    /**
+     * {@code weight()} - returns the weight of this edge
+     * @return weight of this edge
+     */
     public double weight() { return weight; }
 
     @Override
